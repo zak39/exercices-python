d7abd296a3341979101782483d3e8379e8fbcfd0
zakAsur26 <baptiste.fotia.lpasur16-17@gv5.fr>
2017-10-20 00:07:00 +0200

    Boucle pour compter le nombre de mot dans une phrase dans stats_mots_cles.py

--- stats_mots_cles.py
+++ stats_mots_cles.py
@@ -5,8 +5,12 @@ texte = "Le chat noir est sur un arbre noir prés d'un tracteur noir".replace("'
 liste = texte.split()
 doublon_liste = list(set(liste))
 nb = len(doublon_liste)
+compter = 0
 
+for i in range(0,nb):
+    compter = compter + liste.count(doublon_liste[i])
 
 print(liste)
 print(doublon_liste)
 print(nb)
+print(compter)
