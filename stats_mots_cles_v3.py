e857a8255a86830e69cd074f7cc03925678daba8
zak39 <fotia.baptiste@hotmail.com>
2017-10-21 18:35:52 +0200

    Fonction qui retire la ponctuation dans stats_mots_cles_v3.py

---
+++ stats_mots_cles_v3.py
@@ -0,0 +1,70 @@
+# -*- coding:utf8 -*-
+
+#--------- fonctions / méthodes ---------
+
+# Compter le nombre de mot
+def comptage(nb):
+    compter = 0
+    for i in range(0,nb):
+        compter = compter + liste.count(doublon_liste[i])
+    return compter
+
+# Afficher le mot et le nombre d'occurence
+def affiche_occurrence(nb,doublon_liste):
+    for i in range(0,nb):
+        print(str(doublon_liste[i])+" "+str(liste.count(doublon_liste[i])))
+
+# Affiche le taux d'occurence dans un texte
+def pourcentage(doublon_liste,nb,compter):
+    for i in range(0,nb):
+        calcul = round((float(liste.count(doublon_liste[i]))/float(compter))*100,2)
+        print("Le mot '{}' apparait {} sur les {} soit {}% de fois.".format(str(doublon_liste[i]),str(liste.count(doublon_liste[i])),str(nb),str(calcul)))
+
+def deleteElement(liste_parse):
+    rep = True
+    while rep:
+        char = raw_input("\nEst-ce que vous voulez ignorer des mots ? [O]ui [N]on ")
+        if char == "O":
+            liste_ignore = raw_input("\nEcrire votre liste de mot à ignorer en mettant comme séparateur des espaces : ")
+            liste_ignore = liste_ignore.split()
+            liste_parse = [item for item in liste_parse if item not in liste_ignore]
+            rep = False
+        elif char != "N":
+            print("\nProblème dans la saissie.\n")
+        else:
+            rep = False
+    return liste_parse
+
+def deleteCaractere(texte_parse):
+    texte_parse = texte_parse.replace("."," ")    
+    texte_parse = texte_parse.replace("'"," ")
+    texte_parse = texte_parse.replace(","," ")
+    texte_parse = texte_parse.replace('"',' ')
+    texte_parse = texte_parse.replace(':',' ')
+    texte_parse = texte_parse.replace(';',' ')
+    texte_parse = texte_parse.replace('(',' ')
+    texte_parse = texte_parse.replace(')',' ')
+
+    liste_parse = texte_parse.split()
+    liste_caractere = ["?","!"]
+    liste_parse = [item for item in liste_parse if item not in liste_caractere]
+
+    return liste_parse
+
+#--------- main ---------
+
+# Initialisation des variables
+texte = "\nLe chat (noir) est sur un \"arbre\" noir prés d'un tracteur noir.\nMais n'hésite pas à attaquer un hérisson avec ses griffes.\nFort heureusement , un cerf a pu le défendre !"
+
+liste = deleteCaractere(texte)
+
+print("Voici le texte à analyser : \n"+texte)
+
+liste = deleteElement(liste)
+doublon_liste = list(set(liste))
+nombre = len(doublon_liste)
+
+# Appel(le)s fonctions / méthodes
+compt = comptage(nombre)
+affiche_occurrence(nombre,doublon_liste)
+pourcentage(doublon_liste,nombre,compt)
