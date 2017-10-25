8792beeae4fa3f25580f654d1d208c83d86a91e0
zak39 <fotia.baptiste@hotmail.com>
2017-10-25 20:02:38 +0200

    Script python amortissement.py finalisé avec deux fonctions et retourne une matrice

--- armotissement/armotissement.py
+++ armotissement/armotissement.py
@@ -1,52 +1,26 @@
-# -*- coding:utf8 -*-
-# Armotissement = emprunt / nbannuite
-def Armotissement(emprunt,duree):
-    armotissement = emprunt/duree
-    return armotissement
-    pass
+#-*- coding: utf-8 -*-
 
-# interets = emprunt_restant_armortir * taux_interet
-def Interet(armotissement,taux):
-    interet = armotissement * (float(taux)/float(100))
-    return interet
-    pass
+#------------- Fonctions / Methodes -------------
 
-# annuite = armotissement + interet
-def Annuite(armotissement,interet):
-    annuite = armotissement+interet
-    return annuite
-    pass
-
-# valeur_net = emprunt_restant_debut_periode - armotissement_de_annee
-def ValeurNette(emprunt,armotissement_annee):
-    val_nette = emprunt - armotissement_annee
-    return val_nette
-    pass
-
-def CalculArmotissementConstant(interet,armotissement,annuite,val_nette,emprunt_restant,liste):
+# Fonction: Calculer l'amortissement
+def CalculAmortissementConstant(emprunt,annee,taux):
+    liste = [["Annees","Emprunt restant du","interet","Armotissement","Annuite","Valeur nette"]]       # Créer la premiere liste de la matrice. Cette liste représente l'en-tête
+    emprunt_restant = emprunt                                   # On met de coté l'emprunt pour mettre dans la variable emprunt_restant. Cette dernière varie en fonction de l'année
     for i in range(0,annee):
-        interet = Interet(emprunt_restant,taux)
-        armotissement = Armotissement(emprunt,annee)
-        annuite = Annuite(armotissement,interet)
-        val_nette = ValeurNette(emprunt_restant,armotissement)
+        interet = emprunt_restant * (float(taux)/float(100))    # On calcul les intérêts. C'est un coût qui correspond à une utilisation anticipé
+        amortissement = emprunt/annee                           # On calcul le coût de remboursement pour l'année en cours sur le projet
+        annuite = amortissement+interet                         # On calcul l'annuité. C'est le coût que l'on verse annuellement par un emprunteur pour rembourser une dette
+        val_nette = emprunt_restant - amortissement             # C'est la valeur nette du coût du projet dans l'année
 
-        liste = liste+[["Année {}".format(i+2),emprunt_restant,interet,armotissement,annuite,val_nette]]
+        liste = liste+[["Annee {}".format(i+2),emprunt_restant,interet,amortissement,annuite,val_nette]]    # Concaténation d'une liste dans une liste pour former une matrice (liste dans une liste)
 
-        emprunt_restant=val_nette
-
+        emprunt_restant=val_nette                               # On met à jour l'emprunt restant
     return liste
-    pass
 
+# Méthode: Affiche liste par liste notre matrice
 def Affichage(liste):
     for item in liste:
         print(item)
-
-# ce qui est donne :
-#   - emprunt
-#   - duree
-#   - taux
-
-# Utiliser module numpy puis un print pour l'afficher correctement
 
 #----------- main -----------
 
@@ -56,13 +30,5 @@ taux = input("Taux d'intérêt : ")
 
 print("\nVous avez choisi un emprunt d'un montant de {}€ avec un taux d'intérêt de {}% sur {} an(s)\n".format(emprunt,taux,annee))
 
-interet = emprunt*(float(taux)/float(100))
-armotissement = emprunt/annee
-annuite = armotissement+interet
-val_nette = emprunt-armotissement
-emprunt_restant = emprunt
-liste = [["Années","Emprunt restant dû","intérêt","Armotissement","Annuité","Valeur nette"]]
-
-liste = CalculArmotissementConstant(interet,armotissement,annuite,val_nette,emprunt_restant,liste)
-
+liste = CalculAmortissementConstant(emprunt,annee,taux)
 Affichage(liste)
