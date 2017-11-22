ef6a23d9d93ed5df03a9a3fc1c9e0ca1b4e6dd27
zak39 <fotia.baptiste@hotmail.com>
2017-11-22 22:41:46 +0100

    bases-poo: Modification du fichier bases-poo/voiture.py, fonctionnel avec commentaire

--- bases-poo/voiture.py
+++ bases-poo/voiture.py
@@ -14,47 +14,114 @@ class Voiture:
     >>> Voiture("XYZ",2017,"Porsche")
     """
 
-    # Constructeur qui initialise les attributs (un attribut est une caractéristique)
+    # Constructeur qui initialise les attributs (un attribut est une caractéristique lié à l'objet)
     def __init__(self,immat,date,marque):
         self.immatricule = immat    # attribut qui est immatricule de la voiture
-        self.date = date
-        self.marque = marque
+        self.date = date            # attribut qui est la date de création de la voiture
+        self.marque = marque        # attribut qui est la marque de la voiture
 
     # Setter (modifier) la plaque d'immatriculation
     def set_Immat(self,immat):
+        """
+        C'est une "setter" qui permet de modifier l'attribut immatricule.
+        Exemple :\n\n
+        >>> v1.set_Immat("GHJ")
+        >>> print(v1.affiche())
+        ('GHJ', 2007, 'Porsche')
+        """
         self.immatricule = immat
 
     # Setter (modifier) modifier la date
     def set_Date(self,date):
+        """
+        C'est une "setter" qui permet de modifier l'attribut date.
+        Exemple :\n\n
+        >>> v1.set_Date(1991)
+        >>> print(v1.affiche())
+        ('XYZ', 1991, 'Porsche')
+        """
         self.date = date
 
     # Setter (modifier) la marque
     def set_Marque(self,nom_marque):
+        """
+        C'est une "setter" qui permet de modifier l'attribut marque.
+        Exemple :\n\n
+        >>> v1.set_Marque("Renault")
+        >>> print(v1.affiche())
+        ('XYZ', 2007, 'Renault')
+        """
         self.marque = nom_marque
 
-    # Guetteur qui sert uniquement a afficher le nom
+    # Guetteur qui sert uniquement a afficher l'attribut immatricule
     def get_immat(self):
+        """
+        C'est une "getter" qui permet de retourner l'attribut immatricule.
+        Exemple :\n\n
+        >>> v2.get_immat()
+        ABC
+        """
         return self.immatricule
 
+    # Guetteur qui sert uniquement a afficher l'attribut date
     def get_date(self):
+        """
+        C'est une "getter" qui permet de retourner l'attribut date.
+        Exemple :\n\n
+        >>> v2.get_date()
+        1993
+        """
         return self.date
 
+    # Guetteur qui sert uniquement a afficher l'attribut marque
     def get_marque(self):
+        """
+        C'est une "getter" qui permet de retourner l'attribut marque.
+        Exemple :\n\n
+        >>> v2.get_marque()
+        Clio
+        """
         return self.marque
 
-    # Methode qui permet d'afficher les caracteristiques de la voiture
+    # Methode qui permet d'afficher tous les attributs (caractéristiques) de la voiture
     def affiche(self):
+        """
+        C'est une méthode qui permet de retourner les attributs des objets.
+        Exemple :\n\n
+        >>> print(v2.affiche())
+        ('ABC', 1993, 'Clio')
+        """
         return self.immatricule,self.date,self.marque
 
-    # Methode pour savoir si c'est
+    # Methode pour savoir si c'est une voiture de collection
     def estDeCollection(self):
+        """
+        C'est une méthode qui permet de retourner un booléen afin de déterminer si\n
+        une voiture est de collection (avant 1997) ou non (plus de 1997).
+        Exemple :\n\n
+        >>> if v2.estDeCollection():
+        >>>     print("C'est une voiture de collection")
+        >>> else:
+        >>>     print("Ce n'est pas une voiture de collection")
+        C'est une voiture de collection
+        """
         if self.date < 1997:
             return True
         else:
             return False
 
-    # Methode qui permet déterminer si la voiture est de luxe ou pas
+    # Methode qui permet déterminer si la voiture est de luxe
     def estDeLuxe(self):
+        """
+        C'est une méthode qui permet de retourner un booléen afin de déterminer si\n
+        une voiture est de luxe ("Porsche") ou non (qui ne soit pas "Porsche").
+        Exemple:\n\n
+        >>> if v2.estDeLuxe():
+        >>>     print("C'est une voiture de luxe")
+        >>> else:
+        >>>     print("Ce n'est pas une voiture de luxe")
+        Ce n'est pas une voiture de luxe
+        """
         if self.marque == "Porsche":
             return True
         else:
@@ -87,3 +154,12 @@ if v2.estDeLuxe():
     print("C'est une voiture de luxe")
 else:
     print("Ce n'est pas une voiture de luxe")
+
+# Afficher l'attribut immatricule de l'objet (ou instance) v2
+print("Voici l'attribut immatricule de v2 : {}".format(v2.get_immat()))
+
+# Afficher l'attribut date de l'objet (ou instance) v2
+print("Voici l'attribut date de v2 : {}".format(v2.get_date()))
+
+# Afficher l'attribut marque de l'objet (ou instance) v2
+print("Voici l'attribut marque de v2 : {}".format(v2.get_marque()))
